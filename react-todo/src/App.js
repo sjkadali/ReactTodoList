76358652217943346ffd71c3bbbb16099e5ebba9
sjkadali <skadalikat@gmail.com>
2020-07-22 23:47:22 -0400

    First commit

---
+++ react-todo/src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
+import About from './components/pages/About';
+import uuid from 'react-uuid';
+
+import './App.css';
+
+class App extends Component {
+    state = {
+        todos: [
+            {
+                id: uuid(),
+                title: 'Take out the trash',
+                completed: false
+            },
+            {
+                id: uuid(),
+                title: 'Dinner out with family',
+                completed: false
+            },
+            {
+                id: uuid(),
+                title: 'Make Bill payment',
+                completed: false
+            }
+        ]
+    }
+
+    markComplete = (id) => {
+        this.setState({todos: this.state.todos.map(todo => {
+            if(todo.id === id) {
+                todo.completed = !todo.completed
+            }
+            return todo;
+        }) })
+    }
+
+    delTodo = (id) => {
+        this.setState({todos: [...this.state.todos.filter(todo =>
+            todo.id !== id)] });
+    }
+
+    addTodo = (title) => {
+        const newTodo = {
+            id: uuid(),
+            title,
+            completed: false
+        }
+        this.setState({ todos: [...this.state.todos, newTodo] });
+    }
+
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <div className="container">
+                    <Header />
+                    <Route exact path="/" render={props => (
+                        <React.Fragment>
+                            <AddTodo addTodo={this.addTodo} />
+                            <Todos todos={this.state.todos} markComplete={this.markComplete}
+                                delTodo={this.delTodo} />
+                        </React.Fragment>
+                    )} />
+                    <Route path="/about" component={About} />
+                    </div>
+                </div>
+            </Router>
+        )
+    }
+}
+
+export default App;
---
+++ react-todo/src/components/Todos.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import propTypes from 'prop-types';
+
+class Todos extends Component {
+    render() {
+        return this.props.todos.map((todo) => (
+            <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} delTodo={this.props.delTodo} />
+
+        ));
+    }
+}
+
+Todos.propTypes = {
+    todos: propTypes.array.isRequired
+}
+
+export default Todos;
